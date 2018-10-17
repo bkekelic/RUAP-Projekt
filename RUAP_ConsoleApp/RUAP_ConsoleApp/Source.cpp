e161ee6e504497df99bc8c5a7d6f0aa80888041e
bkekelic <berky9@gmail.com>
2018-10-17 14:15:37 +0200

    Update projekta, dodan broj indeksa

--- RUAP_ConsoleApp/RUAP_ConsoleApp/Source.cpp
+++ RUAP_ConsoleApp/RUAP_ConsoleApp/Source.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-	printf("Pozdrav iz aplikacije od Bernarda Kekelica");
+	printf("Pozdrav iz aplikacije od Bernarda Kekelica, broj indeksa = R3787");
 	return 0;
 }
